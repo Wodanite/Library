bf72a5ad5dcd87fca4929f838e9482c6488d9060
Filip <f.golub321@gmail.com>
2021-10-27 15:25:02 +0200

    Modify table and book objects to no longer contain number

--- scripts/main.js
+++ scripts/main.js
@@ -3,30 +3,26 @@ const tableDiv=document.querySelector("#tableDiv");
 const addBookForm=document.createElement("div");
 const addBookButton=document.querySelector("#addBookButton");
 
-function Book(number, title, author, pages, read){
-    this.number=number
+function Book(title, author, pages, read){
     this.title=title
     this.author=author
     this.pages=pages
     this.read=read
 }
 
-function addBookToLibrary(numberInput, titleInput, authorInput, pagesInput, readInput){
-    const book=new Book(numberInput,titleInput, authorInput, pagesInput, readInput);
+function addBookToLibrary(titleInput, authorInput, pagesInput, readInput){
+    const book=new Book(titleInput, authorInput, pagesInput, readInput);
     myLibrary.push(book);
 }
 
 for(let i=0;i<10;i++){
-    addBookToLibrary(i,i+"input","someAuthor",i*5,"yes");
+    addBookToLibrary(i+"input","someAuthor",i*5,"yes");
 }
 
 let table="<table>";
 
 for(let i=0;i<myLibrary.length;i++){
     table+="<tr>";
-    table+="<td>";
-    table+=myLibrary[i].number;
-    table+="</td>"
     table+="<td>";
     table+=myLibrary[i].title;
     table+="</td>"
