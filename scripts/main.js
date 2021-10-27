7d06d20c6016921fd7dd87b45a210600b34051f1
Filip <f.golub321@gmail.com>
2021-10-27 15:39:58 +0200

    Add eventListener to submitButton

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ let myLibrary=[];
 const tableDiv=document.querySelector("#tableDiv");
 const addBookForm=document.createElement("div");
 const addBookButton=document.querySelector("#addBookButton");
+const submitButton=document.querySelector("#submitButton");
 
 function Book(title, author, pages, read){
     this.title=title
@@ -43,6 +44,14 @@ table+="</table>";
 tableDiv.innerHTML=table;
 
 addBookButton.addEventListener("click",()=>{
-    addBookForm.innerHTML='<form><label for="author">Author</label><input type="text" name="author" id="authorInput"><br><label for="title">Title</label><input type="text" name="title" id="titleInput"><br><label for="pages">Number of pages</label><input type="number" name="pages" id="pagesInput"><br><label for="readStatus">Read Status</label><input type="text" name="readStatus" id="readStatusInput"><br><input type="submit" value="Add new Book"></form>';
+    addBookForm.innerHTML='<form><label for="author">Author</label><input type="text" name="author" id="authorInput"><br><label for="title">Title</label><input type="text" name="title" id="titleInput"><br><label for="pages">Number of pages</label><input type="number" name="pages" id="pagesInput"><br><label for="readStatus">Read Status</label><input type="text" name="readStatus" id="readStatusInput"><br><button id="submitButton">Add new Book</button></form>';
     document.body.appendChild(addBookForm);
+});
+
+submitButton.addEventListener("click",()=>{
+    let author=document.querySelector("#authorInput").value;
+    let title=document.querySelector("#titleInput").value;
+    let pages=document.querySelector("#pagesInput").value;
+    let readStatus=document.querySelector("#readStatusInput").value;
+    addBookToLibrary(author,title,pages,readStatus);
 });
